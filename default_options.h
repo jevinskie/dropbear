225bd57826b01b3d989447bac2be8e6dbae37807
Jevin Sweval <jevinsweval@gmail.com>
2018-06-03 01:20:54 -0400

    wip

--- default_options.h
+++ default_options.h
@@ -254,7 +254,10 @@ Homedir is prepended unless path begins with / */
  * OpenSSH), set the path below and set DROPBEAR_SFTPSERVER. 
  * The sftp-server program is not provided by Dropbear itself */
 #define DROPBEAR_SFTPSERVER 1
-#define SFTPSERVER_PATH "/usr/libexec/sftp-server"
+// #define SFTPSERVER_PATH "/usr/libexec/sftp-server"
+// #define SFTPSERVER_PATH "/usr/libexec/sftp-server -l DEBUG3"
+#define SFTPSERVER_PATH "/Users/jevin/code/auto/crv/ssh/nih-sftp-server/prefix/nih-sftp-server"
+// #define SFTPSERVER_PATH "/Users/jevin/code/auto/crv/ssh/nih-sftp-server/xcode/Debug/nih-sftp-server"
 
 /* This is used by the scp binary when used as a client binary. If you're
  * not using the Dropbear client, you'll need to change it */
